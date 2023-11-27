51a6606a13bcbfe063d7d33029a9fc34390fa671
ngocthanh2705 <ngocthanhvu2003@gmail.com>
2023-11-27 08:46:26 +0700

    Update sign up

--- api/customer/signup.php
+++ api/customer/signup.php
@@ -11,34 +11,42 @@
 
     $customer = new customer($connect);
 
-    $data = json_decode(file_get_contents("php://input"));
-    $customer->email = $data->email;
-    $customer->name = $data->name;
-    $customer->password = $data->password;  
-    $customer->phone = $data->phone;
-    $customer->gender = $data->gender;
-    $customer->birthday = $data->birthday;
-    $customer->address = $data->address;
-    $customer->quarter = $data->quarter;
-    $customer->district = $data->district;
-    $customer->city = $data->city;
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-    if ($customer->find()){
-        if($customer->signup()){
-            echo json_encode([
-                'status' => 200,
-                'message' => 'User add Successfully',
-            ]);
+        $data = json_decode(file_get_contents("php://input"));
+        $customer->email = $data->email;
+        $customer->name = $data->name;
+        $customer->password = $data->password;  
+        $customer->phone = $data->phone;
+        $customer->gender = $data->gender;
+        $customer->birthday = $data->birthday;
+        $customer->address = $data->address;
+        $customer->quarter = $data->quarter;
+        $customer->district = $data->district;
+        $customer->city = $data->city;
+
+        if ($customer->find()){
+            if($customer->signup()){
+                echo json_encode([
+                    'status' => 200,
+                    'message' => 'User add Successfully',
+                ]);
+            }else{
+                echo json_encode([
+                    'status' => 400,
+                    'message' => 'Server Problem',
+                ]);
+            }
         }else{
             echo json_encode([
                 'status' => 400,
-                'message' => 'Server Problem',
+                'message' => 'Email already Exists',
             ]);
         }
     }else{
         echo json_encode([
-            'status' => 400,
-            'message' => 'Email already Exists',
+            'status' => 0,
+            'message' => 'Access Denied',
         ]);
     }
 
